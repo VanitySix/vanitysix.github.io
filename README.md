16c4d57c5b9a803668cc384f5d3170387c49e139
Louis Nguyen <sycuown@gmail.com>
2024-12-11 02:40:06 -0800

    Form section is completed

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# webdev-final
+# Webdev Final Project
+
+## Description
+
+## How to run the code
+
+## Deployment
+
+## Sources
+
+Contact Me Form Documentation -> https://www.emailjs.com/docs/tutorial/overview/
+
+Contact Me Form Tutorial -> https://www.youtube.com/watch?v=BgVjild0C9A
+
+Focus Element -> https://developer.mozilla.org/en-US/docs/Web/CSS/:focus
+
+button creation -> https://www.w3schools.com/css/css3_buttons.asp
--- script.js
+++ script.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const sendMail = (event) => {
   // Page wont refresh when user submits
-  event.preventDefault();
+  //event.preventDefault();
 
   const user_data = {
     name: document.getElementById("name").value.trim(),
@@ -21,22 +21,20 @@ const sendMail = (event) => {
       .catch((error) => alert(`Failed to send email: ${error.message}`));
   }
 };
-
+// Validate user input
 const validate_input = ({ name, email, message }) => {
-  // If Any input is empty output error
+  //If input is empty, alert
   if (!name || !email || !message) {
     alert("All fields are required.");
     return false;
   }
 
-  // Uncomment and use this to validate email format if needed
-  /* 
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  // Email validation
+  const emailRegex = /^([a-z\d\.-]+)@([a-z\d]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/;
   if (!emailRegex.test(email)) {
     alert("Please enter a valid email address.");
     return false;
   }
-  */
 
   return true;
 };
